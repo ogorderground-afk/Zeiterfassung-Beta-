6c5dd3221a96e7aa02300559900f65ea7db590d3
ogorderground-afk <og.orderground@gmail.com>
2026-05-12 14:13:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,20 @@
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+ 
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'es2020',
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+})
+ 
